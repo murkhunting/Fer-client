23806cca967ad8e98b180c0f6dc235ba7901606f
Murkhunting <marcmp.arq@gmail.com>
2022-01-06 10:54:28 +0100

    get photo project from id

--- src/pages/photo/Photo.jsx
+++ src/pages/photo/Photo.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./photo.scss";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Photo = () => {
+  const { id } = useParams();
+
+  const [photos, setPhotos] = useState();
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/api/project/${id}`);
+        setPhotos(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPhotos();
+  }, []);
+
+  console.log(photos);
+
   return (
     <div className="photo">
       <h1>SINGLE PHOTO PAGE</h1>
