f001cb9c5bfd02d7b7f413142bedf24c3f524582
murkhunting <marcmp.arq@gmail.com>
2022-01-11 23:28:38 +0100

    navbar menu & refresh button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./navbar.scss";
 import { IoClose } from "react-icons/io5";
 import logo from "../../assets/images/logofer.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
+  const location = useLocation();
+  console.log(location);
+
+  const refresh = () => {
+    if (location.pathname === "/") {
+      window.location.reload(false);
+    }
+  };
 
   const showMenu = () => setMenu(!menu);
 
@@ -25,26 +33,26 @@ const Navbar = () => {
           </div>
           <div className="center">
             <Link to="/" className="link">
-              <img src={logo} alt="" />
+              <img onClick={refresh} src={logo} alt="" />
             </Link>
           </div>
         </div>
         <div className={menu ? "menu active" : "menu"}>
           <div className="menucontainer">
             <Link to="/" className="link">
-              <span onClick={showMenu}>HOME</span>
+              <h1 onClick={showMenu}>HOME</h1>
             </Link>
             <Link to="/videos" className="link">
-              <span onClick={showMenu}>VIDEOS</span>
+              <h1 onClick={showMenu}>VIDEOS</h1>
             </Link>
             <Link to="/photos" className="link">
-              <span onClick={showMenu}>PHOTOS</span>
+              <h1 onClick={showMenu}>PHOTOS</h1>
             </Link>
             <Link to="/how" className="link">
-              <span onClick={showMenu}>HOW I WORK</span>
+              <h1 onClick={showMenu}>HOW I WORK</h1>
             </Link>
             <Link to="/contact" className="link">
-              <span onClick={showMenu}>CONTACT</span>
+              <h1 onClick={showMenu}>CONTACT</h1>
             </Link>
             <div>
               <IoClose className="closeicon" onClick={showMenu} />
